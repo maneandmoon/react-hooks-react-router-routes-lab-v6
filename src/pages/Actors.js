e48d8ef3f30c6951bebfa97572d27ca805df3b50
LindaTa <lindata@maneandmoon.com>
2024-07-26 18:52:07 -0500

    lab completed

--- src/pages/Actors.js
+++ src/pages/Actors.js
@@ -1,13 +1,31 @@
 import { useEffect, useState } from "react";
+import ReusuableCard from "../components/ReusableCard";
+import NavBar from "../components/NavBar";
 
 function Actors() {
+  const [actors, setActors] = useState([]);
+
+  useEffect(() =>{
+    fetch("http://localhost:4000/actors")
+    .then (res => res.json())
+    .then(data => setActors(data))
+    .catch(error => console.error(error))
+  }, [])
+
+  const actorList = actors.map(actor => 
+      <ReusuableCard key={actor.id} name={actor.name} movies={actor.movies} />
+  )
   return (
     <>
       <header>
         {/* What component should go here? */}
+        <NavBar />
       </header>
       <main>
         {/* Actor info here! */}
+        <h1>Actors Page</h1>
+        <ul>{actorList}</ul>
+        
       </main>
     </>
   );
--- src/pages/Directors.js
+++ src/pages/Directors.js
@@ -1,13 +1,30 @@
 import { useEffect, useState } from "react";
+import ReusuableCard from "../components/ReusableCard";
+import NavBar from "../components/NavBar";
 
 function Directors() {
+  const [directors, setDirectors] = useState([]);
+  
+  useEffect(() =>{
+    fetch("http://localhost:4000/directors")
+    .then(res => res.json())
+    .then(data => setDirectors(data))
+    .catch(error => console.error(error))
+  }, [])
+
+  const directorList = directors.map(director => 
+      <ReusuableCard key={director.id} name={director.name} movies={director.movies} />)
+  
   return (
     <>
       <header>
         {/* What component should go here? */}
+        <NavBar />
       </header>
       <main>
         {/* Director info here! */}
+        <h1>Directors Page</h1>
+        <ul>{directorList}</ul>
       </main>
     </>
   );
